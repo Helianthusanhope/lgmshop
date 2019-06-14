af24287ba5b88a0770e921245eacccfcd3b21ec3
lvbo <lvbo_git@163.com>
2019-06-14 18:08:25 +0800

    分配路由行数

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*
+	路由分配
+		
+		吕博: 数据共享(23,24)  	路由分配 32-131
+		
+		关超:数据共享(25,26) 		路由分配 132-232
+		
+		满玉奇:数据共享(27,28)  	 路由分配 233-333
+	
+	数据共享:
 
+		
+	
+		
+		
+
+				
+
+ */
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('admin/index','IndexController@index');
-
+//后台首页 
 Route::resource('admin/index', 'Admin\IndexController');
 
+//后台用户管理
 Route::resource('admin/users', 'Admin\UserController');
+
+//
