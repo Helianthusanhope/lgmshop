c48d503b4c96cd2a4106463167f2e0273c36f737
lvbo <lvbo_git@163.com>
2019-06-22 21:50:34 +0800

    首页跳到活动页面了,个人中心初创型,改header

--- resources/views/home/public/header.blade.php
+++ resources/views/home/public/header.blade.php
@@ -1,14 +1,19 @@
-<!-- header 开始 -->
-    <body>
+<body>
         <div class="hmtop">
             <!--顶部导航条 -->
             <div class="am-container header">
                 <ul class="message-l">
                     <div class="topMessage">
-                        <div class="menu-hd">
+                        <div class="menu-hd"> 
+                            @if(session('home_login'))    
+                            <a href="/home/login" target="_top" class="h">{{session('home_user')->uname}}</a>
+                            <a href="/home/loginout" target="_top">退出</a>
+                            @else
+                                 
                             <a href="/home/login" target="_top" class="h">亲，请登录</a>
                             <a href="/home/register" target="_top">免费注册</a>
-                        </div>
+                            @endif
+                         </div>
                     </div>
                 </ul>
                 <ul class="message-r">
@@ -16,7 +21,7 @@
                         <div class="menu-hd"><a href="/" target="_top" class="h">商城首页</a></div>
                     </div>
                     <div class="topMessage my-shangcheng">
-                        <div class="menu-hd MyShangcheng"><a href="#" target="_top"><i class="am-icon-user am-icon-fw"></i>个人中心</a></div>
+                        <div class="menu-hd MyShangcheng"><a href="/home/myself" target="_top"><i class="am-icon-user am-icon-fw"></i>个人中心</a></div>
                     </div>
                     <div class="topMessage mini-cart">
                         <div class="menu-hd"><a id="mc-menu-hd" href="#" target="_top"><i class="am-icon-shopping-cart  am-icon-fw"></i><span>购物车</span><strong id="J_MiniCartNum" class="h">0</strong></a></div>
@@ -25,4 +30,3 @@
                         <div class="menu-hd"><a href="#" target="_top"><i class="am-icon-heart am-icon-fw"></i><span>收藏夹</span></a></div>
                 </ul>
                 </div>
-<!-- header结束 -->
