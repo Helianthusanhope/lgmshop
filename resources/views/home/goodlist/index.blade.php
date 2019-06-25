dcd7f5a48b0943da8aaf61e164b12bfc44cd8110
lvbo <lvbo_git@163.com>
2019-06-25 09:46:53 +0800

    分类首页小导航

--- resources/views/home/goodlist/index.blade.php
+++ resources/views/home/goodlist/index.blade.php
@@ -36,27 +36,30 @@
 					</form>
 				</div>
 			</div>
+			<div class="nav-table" style="margin-bottom: 50px" >
+						   <div class="long-title"><span class="all-goods">全部分类</span></div>
+						   <div class="nav-cont">
+								<ul>
+									<li class="index"><a href="#">首页</a></li>
+	                                <li class="qc"><a href="#">闪购</a></li>
+	                                <li class="qc"><a href="#">限时抢</a></li>
+	                                <li class="qc"><a href="#">团购</a></li>
+	                                <li class="qc last"><a href="#">大包装</a></li>
+								</ul>
+							    <div class="nav-extra">
+							    	<i class="am-icon-user-secret am-icon-md nav-user"></i><b></b>我的福利
+							    	<i class="am-icon-angle-right" style="padding-left: 10px;"></i>
+							    </div>
+							</div>
+				</div>
 
 			<div class="clear"></div>
-			<b class="line"></b>
+
+			
            <div class="search">
+           	<b class="line"></b>
 			<div class="search-list">
-			<div class="nav-table">
-					   <div class="long-title"><span class="all-goods">全部分类</span></div>
-					   <div class="nav-cont">
-							<ul>
-								<li class="index"><a href="#">首页</a></li>
-                                <li class="qc"><a href="#">闪购</a></li>
-                                <li class="qc"><a href="#">限时抢</a></li>
-                                <li class="qc"><a href="#">团购</a></li>
-                                <li class="qc last"><a href="#">大包装</a></li>
-							</ul>
-						    <div class="nav-extra">
-						    	<i class="am-icon-user-secret am-icon-md nav-user"></i><b></b>我的福利
-						    	<i class="am-icon-angle-right" style="padding-left: 10px;"></i>
-						    </div>
-						</div>
-			</div>
+				
 			
 				
 					<div class="am-g am-g-fixed">
