c914489ee5f087e48ba799fb313cc75208785db3
guanchao1028 <gc_guanchao@163.com>
2019-06-26 21:24:33 +0800

    地址管理90%写好,安全管理雏形

--- app/Http/Controllers/Home/AddressController.php
+++ app/Http/Controllers/Home/AddressController.php
@@ -5,40 +5,139 @@ namespace App\Http\Controllers\Home;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Users;
+use App\Models\ UserAddr;
+use DB;
 
 class AddressController extends Controller
 {
-    //收货地址页面显示
+    //封装查询地址的方法
     
+    public static function address()
+    {
+        //获取数据并分配
+        $uid = session('home_user')->uid;
+        $user = Users::find($uid);
+        // 找到用户的的收货地址
+        return $addr = $user->useraddr;
+
+
+    }
+
+    //收货地址页面显示
     public function index()
     {
-    	//获取数据并分配
-    	$uid = session('home_user')->uid;
-    	$user = Users::find($uid);
-    	// 找到用户的的收货地址
-    	$addr = $user->useraddr;	
+    	
+        $addr = self::address();
+            
     	return view('home.address.index',['addr'=>$addr]);
     }
 
+    //执行添加
     public function store(Request $request)
     {
         $this->validate($request, [
             'addrname' => 'required',
-            'phone' => 'required|regex:/^1{1}[3-9]{1}[\d]{9}$/',
-            'province' => 'required',                
-            'country' => 'required',
-            'town' => 'required',
+            'phone' => 'required|regex:/^1{1}[3-9]{1}[\d]{9}$/',           
             'address' => 'required',
         ],[
             'addrname.required'=>'收货人必填',
             'phone.required'=>'收货人电话',
-            'phone.regex'=>'电话格式不对', 
-            'province.required'=>'省份必填',
-            'country.required'=>'城市必填',
-            'town.required'=>'乡镇必填',
-            'address.required'=>'折扣必填',
+            'phone.regex'=>'请填写正确的手机号',
+            'address.required'=>'地址必填',            
+            
         ]);
-       
+
+        //接收数据
+        $addr = $request->all();
+
+        $province = $addr['province'];
+        $country = $addr['country'];
+        $town = $addr['town'];
+        $addr['area'] = $province.' '. $country.' '.$town;
+        $phone = $addr['phone'];
+        var_dump($phone);      
+        $uid = session('home_user')->uid;
         
+        //执行添加
+        $useraddr = new  UserAddr;
+
+        $useraddr->uid = $uid;
+        $useraddr->address = $addr['address'];
+        $useraddr->addrname = $addr['addrname'];
+        $useraddr->phone = $phone;
+        $useraddr->area = $addr['area'];
+        $useraddr->status = '0';
+
+
+
+
+        $res = $useraddr->save();
+        
+        if($res){
+             return back()->with('success','添加成功');
+        }    
+        
+    }
+
+    //执行修改为默认地址   
+    public function update(Request $request)
+    {
+        
+        //获取需要设置的aid 
+        $aid = $request->aid;    
+        //查询现有的收货地址
+        $addr_all = self::address();
+
+        $status = [];
+        foreach($addr_all as $k=>$v){          
+            //如果有默认地址,就将默认地址清空
+            if($v->aid == $aid && $v->status == '1'){
+               
+                $addr = UserAddr::find( $v->aid );
+                $addr->status ='0';
+                $res = $addr->save();
+
+                if($res){
+
+                return back()->with('success','设置成功'); 
+                }
+                
+                
+            }
+             //接受要修改的地址,设置为默认地址              
+            if($v->aid == $aid && $v->status =='0' ){
+
+                $addr = UserAddr::find( $aid );
+                $addr->status = '1' ;
+                $res = $addr->save();
+
+                if($res){
+                return back()->with('success','设置成功'); 
+            }
+      
+        }
+
+
+                    
+        }
+        
+   
+       
+
+    }
+
+    //执行地址的删除      
+    public function edit(Request $request)
+    {
+        $aid = $request->aid;
+        $addr = UserAddr::find($aid);
+        $res  = $addr->delete();
+
+        if($res){
+
+            return back()->with('success','删除成功'); 
+        }
+
+           
     }
 }
--- resources/views/home/address/index.blade.php
+++ resources/views/home/address/index.blade.php
@@ -32,131 +32,142 @@
 @section('content')
 
 <div class="user-address">
-						<!--标题 -->
+						<!--错误信息-->
+						@if (count($errors) > 0)
+							<div class="am-btn am-btn-danger">
+							    <ul>
+							        @foreach ($errors->all() as $error)
+							            <li>{{ $error }}</li>
+							        @endforeach
+							    </ul>
+							</div>
+    					@endif
+						<!--提示成功信息-->
+    					@if(session('success'))
+		                 <div class="am-cf am-padding">
+		                 <ul>		             
+		                 	<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg" style="background:fuchsia;color:$ccc">{{ session('success') }}</span></strong><small></small></div>
+		                 </ul>		                 	
+		                   
+		                 </div>
+                 		@endif 
+
 						<div class="am-cf am-padding">
 							<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg">地址管理</strong><small></small></div>
 						</div>
 						<hr/>
-						<ul class="am-avg-sm-1 am-avg-md-3 am-thumbnails">
-							@foreach($addr as  $k=>$v)
-							<li class="user-addresslist">
-								<span class="new-option-r"><i class="am-icon-check-circle"></i>设为默认</span>
-								<p class="new-tit new-p-re">
-									<span class="new-txt">{{ $v->addrname }}</span>
-									<span class="new-txt-rd2">{{ $v->phone }}</span>
-								</p>
-								<div class="new-mu_l2a new-p-re">
-									<p class="new-mu_l2cw">
-										<span class="title">详细地址：</span>									
-										<span class="street">{{ $v->address}}</span></p>
-								</div>
-
-								<div class="new-addr-btn">
-									<a href="#"><i class="am-icon-edit"></i>编辑</a>
-									<span class="new-addr-bar">|</span>
-									<a href="javascript:void(0);" onClick="delClick(this);"><i class="am-icon-trash"></i>删除</a>
-								</div>
-							</li>
-							@endforeach
-						</ul>
-						
-						<div class="clear"></div>
-						<a class="new-abtn-type" data-am-modal="{target: '#doc-modal-1', closeViaDimmer: 0}">添加新地址</a>
-						<!--例子-->
-						<div class="am-modal am-modal-no-btn" id="doc-modal-1">
-
-							<div class="add-dress">
-	
-
-								<div class="am-cf am-padding">
-									<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg">新增地址</strong> <small></small></div>
-								</div>
-								<hr/>
+						@foreach($addr as  $k=>$v)
+						<ul>						
 								
-								<div class="am-u-md-12 am-u-lg-8" style="margin-top: 20px;">
-									<form action="/home/address/store"class="am-form am-form-horizontal" method="post">
-										{{ csrf_field() }}
-
-										<div class="am-form-group">
-											<label for="user-name" class="am-form-label">收货人</label>
-											<div class="am-form-content">
-												<input type="text" name="addrname" placeholder="收货人">
-											</div>
-										</div>
-
-										<div class="am-form-group">
-											<label for="user-phone" class="am-form-label">手机号码</label>
-											<div class="am-form-content">
-												<input id="user-phone" name="phone" placeholder="手机号必填" type="email">
-											</div>
-										</div>
-										<div class="am-form-group">
-											<label for="user-address" class="am-form-label">所在地</label>
-											<script src="/ho/area.js"></script>
-											<div class="am-form-content address">
-												<select id="Province" runat="server" name="province" style="width: 90px" ></select>
-												<select id="Country" runat="server" name="country" style="width: 90px"></select>
-												<select id="Town" runat="server" name="town" style="width: 90px"></select>
-											</div>
+								@if($v->status)			
+									<li class="user-addresslist">										
+										<p class="new-tit new-p-re">
 											
+											<span class="new-txt">{{ $v->addrname }}</span>
+											<span class="new-txt-rd2">{{ $v->phone }}</span>
+										</p>
+
+										<div class="new-mu_l2a new-p-re">
+											<p class="new-mu_l2cw">
+												<span class="title">地址：</span>
+												<span class="province">{{ $v->area}}</span>
+												<span class="street">{{ $v->address}}</span></p>
 										</div>
-										<script language="javascript">
+										<div class="new-mu_l2cw">
+											<a href="/home/address/update/{{$v->aid}}"><i class="am-icon-edit" style="background:yellow">取消默认地址</i></a>
+											<span class="new-mu_l2cw">|</span>
+											<a href="/home/address/edit/{{$v->aid}}"><i style="background:#ccc">删除</i></a>
+										</div>
+									</li>
+								@else
+									<li class="user-addresslist">										
+									<p class="new-tit new-p-re">
+										
+										<span class="new-txt">{{ $v->addrname }}</span>
+										<span class="new-txt-rd2">{{ $v->phone }}</span>
+									</p>
+
+									<div class="new-mu_l2a new-p-re">
+										<p class="new-mu_l2cw">
+											<span class="title">地址：</span>
+											<span class="province">{{ $v->area}}</span>
+											<span class="street">{{ $v->address}}</span></p>
+									</div>
+									<div class="new-mu_l2cw">
+										<a href="/home/address/update/{{$v->aid}}"><i class="am-icon-edit"></i>设置为默认地址</a>
+										<span class="new-mu_l2cw">|</span>
+										<a href="/home/address/edit/{{$v->aid}}"><i style="background:#ccc">删除</i></a>
+									</div>
+								</li>
+								@endif
+							
+						</ul>
+						@endforeach
+						<div class="clear"></div>   					
+					<div class="am-cf am-padding">
+							<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg">新建地址</strong><small></small></div>
+						</div>
+						<div class="info-main">
+							
+							<form  action= "/home/address/store"class="am-form am-form-horizontal" method="post">
+
+								
+								{{ csrf_field() }}
+								<div class="layui-form-item" style="width:300px">
+									<label for="user-name" class="layui-form-label"> 收货人</label>
+									<div class="layui-input-inline">
+										<input type="text" name="addrname" required value="{{ old('addrname') }}" class="layui-input" lay-verify="required" >
+
+									</div>
+								</div>
+								<div class="layui-form-item" style="width:300px">
+									<label for="user-name" class="layui-form-label">收货人电话</label>
+									<div class="layui-input-inline"">
+										<input type="text" name="phone"  value="" required lay-verify="required" class="layui-input" placeholder="请填入正确的11位手机号">
+
+									</div>
+								</div>
+								<div class="layui-form-item" style="width:300px">
+									<label class="layui-form-label">所在地</label>
+									<script src="/ho/area.js"></script>
+									<div class="layui-input-inline">						
+										<div lay-verify="required">
+												<select id="Province" runat="server" name="province" style="width: 200px" lay-verify="required"></select>
+												<select id="Country" runat="server" name="country" style="width: 200px" lay-verify="required"></select>
+												<select id="Town" runat="server" name="town" style="width: 200px" lay-verify="required"></select>
+										</div>
+
+									</div>
+								</div>
+								<script language="javascript">
 												setup();
 											</script>
+													
+								<div class="layui-form-item" style="width:300px">
+									<label for="user-name2" class="layui-form-label">详细地址</label>
+									<div class="layui-input-inline">
+										<input type="text" name="address"  value=" {{old('address')}} " required lay-verify="required" lay-verify="required" >
 
-
-										<div class="am-form-group">
-											<label for="user-intro" class="am-form-label">详细地址</label>
-											<div class="am-form-content">
-												<textarea class="" rows="3" id="user-intro" placeholder="输入详细地址" name="address"></textarea>
-												<small>请填写50字以内</small>
-											</div>
-										</div>
-
-										<div class="am-form-group">
-											
-											<input type="submit" value="保存"class="am-btn am-btn-danger" >										
-								
-										</div>
-									</form>
+									</div>
 								</div>
+								
+								<div class="info-btn">
+								
+									<input type="submit" value="保存" class="am-btn am-btn-danger">
+								</div>
+							 
 
-							</div>
-
-						</div>
-
+							</form>
+							
+						</div>					
 					</div>
 
-					<!--错误提示信息开始-->
-					<script>
-						//一般直接写在一个js文件中
-						layui.use(['layer', 'form'], function(){
-						  var layer = layui.layer
-						  ,form = layui.form;
-						  
-						  layer.msg('Hello World');
-						});
-						</script>
-
+					<!--错误提示信息开始-->					
+					
 					<!--提示信息结束-->
+					
+					
 
-
-
-					<!--模板自带的JS--->
-					<script type="text/javascript">
-						$(document).ready(function() {							
-							$(".new-option-r").click(function() {
-								$(this).parent('.user-addresslist').addClass("defaultAddr").siblings().removeClass("defaultAddr");
-							});
-							
-							var $ww = $(window).width();
-							if($ww>640) {
-								$("#doc-modal-1").removeClass("am-modal am-modal-no-btn")
-							}
-							
-						})
-					</script>
-					<!--模板自带模板结束-->
-
+					
 @endsection
 
