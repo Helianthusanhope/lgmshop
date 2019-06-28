42f15ff3a81067ca7af03ab87ab07578055645b8
lvbo <lvbo_git@163.com>
2019-06-28 09:46:04 +0800

    轮播图显示全图

--- resources/views/home/public/banners.blade.php
+++ resources/views/home/public/banners.blade.php
@@ -2,8 +2,10 @@
                       <!--轮播图开始-->
                         <div class="am-slider am-slider-default scoll" data-am-flexslider id="demo-slider-0">
                             <ul class="am-slides">
+                                <?php $a = 0; ?>
                                 @foreach( $banners_data as $k=>$v )
-                                <li style="background-color:#D2364C;"  class="banner1"><a href="introduction.html"><img style="width:63%;" src="/uploads/{{ $v->url }}" /></a></li>
+                                <?php $a++; ?>
+                                <li class="banner<?php echo "$a"; ?>"><?php if ($a == 1) { echo "\<a href=\"introduction.html\"\>"; } ?><img style="height: 100%" src="/uploads/{{ $v->url }}" /></a></li>
                                 @endforeach
                             </ul>
                         </div>
